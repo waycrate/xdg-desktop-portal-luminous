43f58f7f1b986f916333d4742ef4d99f27fea9d3
Shinyzenith <aakashsensharma@gmail.com>
2023-08-22 17:28:27 +0530

    chore: Add development flake
    Signed-off-by: Shinyzenith <aakashsensharma@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "xdg-desktop-portal-luminous devel";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgsFor = system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
+
+      targetSystems = [ "aarch64-linux" "x86_64-linux" ];
+    in {
+      devShells = nixpkgs.lib.genAttrs targetSystems (system:
+        let pkgs = pkgsFor system;
+        in {
+          default = pkgs.mkShell {
+            name = "xdg-desktop-portal-luminous-devel";
+            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+            nativeBuildInputs = with pkgs; [
+              # Compilers
+              clang
+              cargo
+              rustc
+
+              # Libs
+              pipewire
+              wayland
+              stdenv
+
+              # Tools
+              meson
+              ninja
+              gdb
+              pkg-config
+              rust-analyzer
+              rustfmt
+              strace
+              valgrind
+              wayland-scanner
+            ];
+          };
+        });
+    };
+}
