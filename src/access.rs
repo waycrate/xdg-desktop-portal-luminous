1eec22eca82561d2c6bc1ee3f303c228d1dfac40
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-10-06 10:32:26 +0800

    chore: add accessOption

--- src/access.rs
+++ src/access.rs
@@ -2,10 +2,30 @@ use std::collections::HashMap;
 
 use zbus::{
     dbus_interface, fdo,
-    zvariant::{ObjectPath, OwnedValue, Value},
+    zvariant::{DeserializeDict, ObjectPath, OwnedValue, SerializeDict, Type},
 };
 
+use serde::{Deserialize, Serialize};
+
 use crate::PortalResponse;
+#[derive(SerializeDict, DeserializeDict, Type, Debug, Default)]
+/// Specified options for a [`Screencast::select_sources`] request.
+#[zvariant(signature = "dict")]
+pub struct AccessOption {
+    /// A string that will be used as the last element of the handle.
+    /// What types of content to record.
+    pub modal: Option<bool>,
+    /// Whether to allow selecting multiple sources.
+    pub deny_label: Option<String>,
+    /// Determines how the cursor will be drawn in the screen cast stream.
+    pub grant_label: Option<String>,
+    pub icon: Option<String>,
+    pub choices: Option<Vec<Choice>>,
+}
+
+#[derive(Clone, Serialize, Deserialize, Type, Debug)]
+/// Presents the user with a choice to select from or as a checkbox.
+pub struct Choice(String, String, Vec<(String, String)>, String);
 
 #[derive(Debug)]
 pub struct AccessBackend;
@@ -21,7 +41,7 @@ impl AccessBackend {
         _title: String,
         _sub_title: String,
         _body: String,
-        _options: HashMap<String, Value<'_>>,
+        _options: AccessOption,
     ) -> fdo::Result<PortalResponse<HashMap<String, OwnedValue>>> {
         Ok(PortalResponse::Success(HashMap::new()))
     }
