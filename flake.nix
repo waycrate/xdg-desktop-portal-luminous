90ac3edfcf3a896c6823feb02e2578a8b160e3a5
Rishik-Y <73787402+Rishik-Y@users.noreply.github.com>
2025-03-13 11:45:31 +0530

    Updated flake.nix

--- flake.nix
+++ flake.nix
@@ -1,51 +1,17 @@
 {
-  description = "xdg-desktop-portal-luminous devel";
+  description = "Development environment for wayshot";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      pkgsFor = system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ ];
-        };
-
-      targetSystems = [ "aarch64-linux" "x86_64-linux" ];
-    in {
-      devShells = nixpkgs.lib.genAttrs targetSystems (system:
-        let pkgs = pkgsFor system;
-        in {
-          default = pkgs.mkShell {
-            name = "xdg-desktop-portal-luminous-devel";
-            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-            nativeBuildInputs = with pkgs; [
-              # Compilers
-              clang
-              cargo
-              rustc
-
-              # Libs
-              pipewire
-              wayland
-              libxkbcommon
-              stdenv
-              glib
-              pango
-              cairo
-
-              # Tools
-              meson
-              ninja
-              gdb
-              pkg-config
-              rust-analyzer
-              rustfmt
-              strace
-              valgrind
-              wayland-scanner
-            ];
-          };
-        });
-    };
+  outputs =
+    {
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    {
+      devShells.default = import ./shell.nix;
+    });
 }
