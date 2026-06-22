e4da06f2daf037ab3eeeeefdde873cfb7b6acf8d
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-06-22 10:54:49 +0900

    chore: finish dbus part

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,5 +1,8 @@
 mod wayland_backend;
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{Arc, LazyLock},
+};
 
 use serde::{Deserialize, Serialize};
 use zbus::{
@@ -9,9 +12,32 @@ use zbus::{
 };
 
 use crate::{
+    clipboard::wayland_backend::ClipboardRequest,
     request::RequestInterface,
     session::{Session, SessionType, append_session},
 };
+use tokio::sync::Mutex;
+
+use wayland_backend::ClipboardThread;
+
+pub static CLIPBOARD_SESSION: LazyLock<Arc<Mutex<HashMap<OwnedObjectPath, ClipboardThread>>>> =
+    LazyLock::new(|| Arc::new(Mutex::new(HashMap::new())));
+
+pub async fn append_clipboard_session(object_path: ObjectPath<'_>, thread: ClipboardThread) {
+    let mut sessions = CLIPBOARD_SESSION.lock().await;
+    if let Some(thread) = sessions.insert(object_path.into(), thread) {
+        thread.stop();
+    }
+}
+
+pub async fn remove_clipboard_session(object_path: ObjectPath<'_>) {
+    let mut sessions = CLIPBOARD_SESSION.lock().await;
+    let Some(thread) = sessions.remove(&object_path) else {
+        return;
+    };
+    thread.stop();
+    tracing::info!("session {} is stopped", object_path);
+}
 
 #[derive(Debug, Serialize, Type, Deserialize)]
 #[zvariant(signature = "dict")]
@@ -60,6 +86,8 @@ impl Clipboard {
             .await?;
         let current_session = Session::new(session_handle.clone(), SessionType::Clipboard);
         append_session(current_session.clone()).await;
+        let clipboard_thread = ClipboardThread::new();
+        append_clipboard_session(session_handle, clipboard_thread).await;
         Ok(())
     }
 
@@ -68,15 +96,43 @@ impl Clipboard {
         session_handle: ObjectPath<'_>,
         options: SelectionOpt,
     ) -> zbus::fdo::Result<()> {
-        todo!()
+        let sessions = CLIPBOARD_SESSION.lock().await;
+        let Some(thread) = sessions.get(&session_handle) else {
+            return Err(zbus::fdo::Error::Failed(format!(
+                "no such path: {session_handle}"
+            )));
+        };
+        thread
+            .sender
+            .send(ClipboardRequest::SetSelection {
+                mime_types: options.mime_types,
+            })
+            .map_err(|e| zbus::fdo::Error::Failed(format!("request selection failed: {e}")))?;
+        Ok(())
     }
 
     async fn selection_write(
         &self,
         session_handle: ObjectPath<'_>,
         serial: u32,
+        #[zbus(signal_emitter)] ctx: SignalEmitter<'_>,
     ) -> zbus::fdo::Result<Fd<'_>> {
-        todo!()
+        let sessions = CLIPBOARD_SESSION.lock().await;
+        let Some(thread) = sessions.get(&session_handle) else {
+            return Err(zbus::fdo::Error::Failed(format!(
+                "no such path: {session_handle}"
+            )));
+        };
+        let (sender, receiver) = tokio::sync::oneshot::channel();
+        thread
+            .sender
+            .send(ClipboardRequest::Write { sender, serial })
+            .map_err(|e| zbus::fdo::Error::Failed(format!("request selection failed: {e}")))?;
+        let (fd, mime_type) = receiver
+            .await
+            .map_err(|e| zbus::fdo::Error::Failed(format!("request selection failed: {e}")))?;
+        let _ = Self::session_transfer(&ctx, mime_type.as_str(), serial).await;
+        Ok(Fd::from(fd))
     }
 
     async fn selection_write_done(
@@ -85,7 +141,8 @@ impl Clipboard {
         serial: u32,
         success: bool,
     ) -> zbus::fdo::Result<()> {
-        todo!()
+        // TODO: what to do here?
+        Ok(())
     }
 
     async fn selection_read(
@@ -93,7 +150,24 @@ impl Clipboard {
         session_handle: ObjectPath<'_>,
         mime_type: &'_ str,
     ) -> zbus::fdo::Result<Fd<'_>> {
-        todo!()
+        let sessions = CLIPBOARD_SESSION.lock().await;
+        let Some(thread) = sessions.get(&session_handle) else {
+            return Err(zbus::fdo::Error::Failed(format!(
+                "no such path: {session_handle}"
+            )));
+        };
+        let (sender, receiver) = tokio::sync::oneshot::channel();
+        thread
+            .sender
+            .send(ClipboardRequest::Read {
+                sender,
+                mime_type: mime_type.to_owned(),
+            })
+            .map_err(|e| zbus::fdo::Error::Failed(format!("request selection failed: {e}")))?;
+        let fd = receiver
+            .await
+            .map_err(|e| zbus::fdo::Error::Failed(format!("request selection failed: {e}")))?;
+        Ok(Fd::from(fd))
     }
 
     #[zbus(signal)]
--- src/clipboard/wayland_backend.rs
+++ src/clipboard/wayland_backend.rs
@@ -19,6 +19,24 @@ use wayland_client::{
     globals::registry_queue_init, protocol::wl_seat::WlSeat,
 };
 
+pub struct ClipboardThread {
+    pub sender: Sender<ClipboardRequest>,
+}
+
+impl ClipboardThread {
+    pub fn new() -> Self {
+        let (sender, receiver) = channel::channel();
+        std::thread::spawn(move || {
+            let _ = clipboard_loop(receiver);
+        });
+        Self { sender }
+    }
+
+    pub fn stop(&self) {
+        let _ = self.sender.send(ClipboardRequest::Stop);
+    }
+}
+
 pub struct ClipboardWl {
     registry_state: RegistryState,
     seat: WlSeat,
@@ -44,9 +62,7 @@ impl Dispatch<ext_data_control_device_v1::ExtDataControlDeviceV1, ()> for Clipbo
                 state.reset_offer();
                 state.reset_data_device();
             }
-            ext_data_control_device_v1::Event::Selection { id } => {
-
-            }
+            ext_data_control_device_v1::Event::Selection { id } => {}
             ext_data_control_device_v1::Event::PrimarySelection { id } => {
                 // NOTE: we need to manager its lifetime
                 state.reset_primary_offer(id);
@@ -101,9 +117,19 @@ delegate_noop!(ClipboardWl: ignore ExtDataControlManagerV1);
 sctk::delegate_registry!(ClipboardWl);
 
 pub enum ClipboardRequest {
-    SetSelection { mime_types: Vec<String> },
-    Write { sender: OneSender<OwnedFd> },
-    Read { sender: OneSender<OwnedFd> },
+    SetSelection {
+        mime_types: Vec<String>,
+    },
+    Write {
+        sender: OneSender<(OwnedFd, String)>,
+        serial: u32,
+    },
+
+    Read {
+        sender: OneSender<OwnedFd>,
+        mime_type: String,
+    },
+    Stop,
 }
 
 impl ClipboardWl {
@@ -129,49 +155,53 @@ impl ClipboardWl {
         self.device.set_selection(Some(&source));
         Ok(())
     }
-    pub fn new(receiver: Channel<ClipboardRequest>) -> anyhow::Result<()> {
-        let connection = Connection::connect_to_env()?;
-        let (globals, event_queue) = registry_queue_init::<Self>(&connection)?;
-        let qh = event_queue.handle();
-        let seat = globals.bind::<WlSeat, _, _>(&qh, 1..=1, ())?;
-        let data_manager = globals.bind::<ExtDataControlManagerV1, _, _>(&qh, 1..=1, ())?;
-        let device = data_manager.get_data_device(&seat, &qh, ());
-        let mut clipbard = ClipboardWl {
-            registry_state: RegistryState::new(&globals),
-            seat,
-            data_manager,
-            device,
-            qh,
-            current_selection: None,
-            primary_selection: None,
-        };
+}
+pub fn clipboard_loop(receiver: Channel<ClipboardRequest>) -> anyhow::Result<()> {
+    let connection = Connection::connect_to_env()?;
+    let (globals, event_queue) = registry_queue_init::<ClipboardWl>(&connection)?;
+    let qh = event_queue.handle();
+    let seat = globals.bind::<WlSeat, _, _>(&qh, 1..=1, ())?;
+    let data_manager = globals.bind::<ExtDataControlManagerV1, _, _>(&qh, 1..=1, ())?;
+    let device = data_manager.get_data_device(&seat, &qh, ());
+    let mut clipbard = ClipboardWl {
+        registry_state: RegistryState::new(&globals),
+        seat,
+        data_manager,
+        device,
+        qh,
+        current_selection: None,
+        primary_selection: None,
+    };
 
-        let mut event_loop: EventLoop<ClipboardWl> =
-            EventLoop::try_new().expect("Failed to initialize the event loop");
+    let mut event_loop: EventLoop<ClipboardWl> =
+        EventLoop::try_new().expect("Failed to initialize the event loop");
 
-        WaylandSource::new(connection, event_queue)
-            .insert(event_loop.handle())
-            .expect("Failed to init wayland source");
-        event_loop
-            .handle()
-            .insert_source(receiver, move |event, _, app_state| {
-                let channel::Event::Msg(message) = event else {
-                    return;
-                };
+    let signal = event_loop.get_signal();
+    WaylandSource::new(connection, event_queue)
+        .insert(event_loop.handle())
+        .expect("Failed to init wayland source");
+    event_loop
+        .handle()
+        .insert_source(receiver, move |event, _, app_state| {
+            let channel::Event::Msg(message) = event else {
+                return;
+            };
 
-                match message {
-                    ClipboardRequest::SetSelection { mime_types } => {}
-                    _ => {}
+            match message {
+                ClipboardRequest::SetSelection { mime_types } => {}
+                ClipboardRequest::Stop => {
+                    signal.stop();
                 }
-            })
-            .expect("Error during event loop");
-        event_loop
-            .run(
-                std::time::Duration::from_millis(20),
-                &mut clipbard,
-                |_data| {},
-            )
-            .expect("Error during event loop");
-        Ok(())
-    }
+                _ => {}
+            }
+        })
+        .expect("Error during event loop");
+    event_loop
+        .run(
+            std::time::Duration::from_millis(20),
+            &mut clipbard,
+            |_data| {},
+        )
+        .expect("Error during event loop");
+    Ok(())
 }
